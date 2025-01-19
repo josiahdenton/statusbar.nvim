12f2f2ee4de538bd67f8a29654f25ecb0d5a6b62
Josiah Denton <jfranklindenton@gmail.com>
2025-01-19 08:34:14 -0500

    chore: rmv statusline

--- lua/statusbar/init.lua
+++ lua/statusbar/init.lua
@@ -1,29 +1,9 @@
 local M = {}
 
-local line = require("statusbar.line")
 local bar = require("statusbar.bar")
 
---- @class statusbar.Config
---- @field use_winbar boolean
---- @field use_statusline boolean
-
---- @type statusbar.Config
-local default_config = {
-	use_winbar = true,
-	use_statusline = true,
-}
-
---- @param opts ?statusbar.Config
-M.setup = function(opts)
-	opts = opts or default_config
-
-	if opts.use_winbar ~= nil and opts.use_winbar then
-		bar.setup()
-	end
-
-	if opts.use_statusline ~= nil and opts.use_statusline then
-		line.setup()
-	end
+M.setup = function()
+	bar.setup()
 end
 
 return M
