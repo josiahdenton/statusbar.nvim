47ddd3735f4f8f93f6d90c3f1fec57ed859a62e2
Josiah Denton <jfranklindenton@gmail.com>
2025-06-22 09:30:43 -0400

    chore: rmv stat

--- lua/statusbar/segments/git.lua
+++ lua/statusbar/segments/git.lua
@@ -5,12 +5,10 @@ local store = require("statusbar.store")
 local DRIFT_CACHE_UPDATE_TIME = 30000
 local cache = {
 	branch = nil,
-	stat = nil,
 	drift = { origin = 0, head = 0 }, -- from
 }
 local valid = {
 	branch = false,
-	stat = false,
 	drift = false,
 }
 
@@ -66,73 +64,15 @@ local branch = function()
 	return cache.branch and ("  " .. cache.branch) or ""
 end
 
---- @alias tabby.ChangeType "file"|"ins"|"del"
-
---- @type table<tabby.ChangeType, string>
-local symbols = {
-	file = "",
-	ins = "",
-	del = "",
-}
-
---- @return tabby.ChangeType|nil,string|nil
-local change_symbol = function(change)
-	if string.find(change, "file") then
-		return symbols.file, "markdownUrl"
-	elseif string.find(change, "ins") then
-		return symbols.ins, "DiagnosticOk"
-	elseif string.find(change, "del") then
-		return symbols.del, "DiagnosticError"
-	end
-end
-
---- @return table<table<string>>
-local stat = function()
-	if cache.stat and valid.stat then
-		return cache.stat
-	end
-
-	vim.system({ "git", "diff", "--stat" }, { text = true }, function(out)
-		if #out.stdout > 0 then
-			local lines = vim.split(out.stdout, "\n", { trimempty = true })
-			local changes = lines[#lines]
-			if changes and #changes > 0 then
-				local changes_by_type = vim.split(changes, ",", { trimempty = true })
-				local content = {}
-				local total = 0
-				for _, change in ipairs(changes_by_type) do
-					local amount = string.match(change, "%d+")
-					total = total + tonumber(vim.trim(amount))
-					local symbol, hg = change_symbol(change)
-					table.insert(content, { " ", "Comment" })
-					table.insert(content, { amount, "Comment" })
-					table.insert(content, { " ", "Comment" })
-					table.insert(content, { symbol, hg })
-					table.insert(content, { " ", "Comment" })
-				end
-				if total > 0 then
-					cache.stat = content
-				else
-					cache.stat = {}
-				end
-			end
-		else
-			cache.stat = {}
-		end
-		valid.stat = true
-	end)
-	return cache.stat or {}
-end
-
 M.setup = function()
 	store.register_segment({
 		name = "git",
 		split = false,
 		focused = function()
-			return { { branch, "MiniIconsGreen" }, { drift, "Comment" }, unpack(stat()) }
+			return { { branch, "MiniIconsGreen" }, { drift, "Comment" } }
 		end,
 		default = function()
-			return { { branch, "MiniIconsGreen" }, { drift, "Comment" }, unpack(stat()) }
+			return { { branch, "MiniIconsGreen" }, { drift, "Comment" } }
 		end,
 	})
 
@@ -152,14 +92,6 @@ M.setup = function()
 		callback = vim.schedule_wrap(function()
 			valid.branch = false
 			valid.drift = false
-			valid.stat = false
-		end),
-	})
-
-	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
-		group = group,
-		callback = vim.schedule_wrap(function()
-			valid.stat = false
 		end),
 	})
 
